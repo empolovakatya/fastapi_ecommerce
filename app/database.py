b3f10722603947e15316b54a4f5fcf3d651861f1
ekaterina <empolova_katya@mail.ru>
2025-11-30 11:57:55 +0500

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+# Строка подключения для SQLite
+DATABASE_URL = "sqlite:///ecommerce.db"
+
+# Создаём Engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+# Определяем базовый класс для моделей
+class Base(DeclarativeBase):  # New
+    pass
