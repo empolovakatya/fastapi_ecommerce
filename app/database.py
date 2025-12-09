d98f5072e888a2e3e4fb96f73365a235d99bb7ac
ekaterina <empolova_katya@mail.ru>
2025-12-09 12:28:29 +0500

    Added config for postgres and alembic migrations

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
@@ -9,6 +10,19 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(bind=engine)
 
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = config("DATABASE_URL")
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 # Определяем базовый класс для моделей
 class Base(DeclarativeBase):  # New
